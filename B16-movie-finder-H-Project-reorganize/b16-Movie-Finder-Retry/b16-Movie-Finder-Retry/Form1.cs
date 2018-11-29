02a2f313b9a10801156c0c88c69b78d1ef5d2905
banskin <leminwilliam@gmail.com>
2018-11-29 09:31:22 +0000

    moving from staging to main
    Moving current main project from staging branch to main branch, overseen by WL

---
+++ B16-movie-finder-H-Project-reorganize/b16-Movie-Finder-Retry/b16-Movie-Finder-Retry/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movieFinder
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			this.ActiveControl = textBox1;
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			pictureBox1.Image = null;
+
+			string UrlText = textBox1.Text.Replace(" ", "+");
+
+
+			string URL = "http://www.omdbapi.com/?t=" + UrlText + "&apikey=3c1d34eb";
+
+			WebRequest request = WebRequest.Create(URL);
+			WebResponse response = request.GetResponse();
+			string result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+			string edited_text = result.Replace("\"", "");
+			string edited_text2 = edited_text.Replace("{", "");
+			string edited_text3 = edited_text.Replace("}", "");
+
+
+
+			List<string> TagIds = edited_text2.Split(',').ToList<string>();
+			foreach (string s in TagIds)
+			{
+				listBox1.Items.Add(s);
+
+			}
+			int x = -1;
+			// Loop through and find each item that matches the search string.
+
+			try
+			{
+				// Retrieve the item based on the previous index found. Starts with -1 which searches start.
+				x = listBox1.FindString("Poster");
+
+				string poster = listBox1.Items[x].ToString();
+
+				poster = poster.Remove(0, 7);
+				pictureBox1.Load(poster);
+			}
+			catch
+			{
+				listBox1.Items.Add("NO POSTER FOUND");
+			}
+
+			//infoArea.Text = result;
+
+
+		}
+
+		
+		static Form2 f2 = new Form2();
+		public void button3_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			
+			f2.Show();
+		}
+
+		private void button4_Click(object sender, EventArgs e) //Add to wishlist
+		{
+			int x = -1;
+
+			x = listBox1.FindString("Title");
+
+			string poster = listBox1.Items[x].ToString();
+
+			poster = poster.Remove(0, 6);
+			f2.listBox2.Items.Add(poster);
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+		private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+			// Determine whether the key entered is the F1 key. If it is, display Help.
+			if (e.KeyCode == Keys.Enter)
+			{
+				button1.PerformClick();
+			}
+
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ B16-movie-finder-H-Project-reorganize/b16-Movie-Finder-Retry/b16-Movie-Finder-Retry/Program.cs
@@ -0,0 +1,23 @@
+﻿using movieFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b16_Movie_Finder_Retry
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
