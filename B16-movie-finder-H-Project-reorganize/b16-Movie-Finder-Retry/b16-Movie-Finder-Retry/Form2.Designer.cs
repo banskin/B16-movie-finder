f359e1c3901398a0178029d358fbd1022f1c7b5c
banskin <leminwilliam@gmail.com>
2018-11-29 09:27:36 +0000

    Added merged files to staging for push to master
    checked by pair and published to staging

---
+++ B16-movie-finder-H-Project-reorganize/b16-Movie-Finder-Retry/b16-Movie-Finder-Retry/Form2.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace movieFinder
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.Location = new System.Drawing.Point(32, 119);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(502, 368);
+			this.listBox2.TabIndex = 0;
+			this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(32, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Search";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(301, 51);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(189, 20);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(651, 50);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(106, 46);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(862, 508);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox2);
+			this.Name = "Form2";
+			this.Text = "Form2";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		public System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ B16-movie-finder-H-Project-reorganize/b16-Movie-Finder-Retry/b16-Movie-Finder-Retry/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movieFinder
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+		static Form1 f1 = new Form1();
+		public void button2_Click(object sender, EventArgs e)
+		{
+			this.Hide(); // This is bad
+			f1.Show();
+		}
+
+		public void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		public void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		public void button1_Click(object sender, EventArgs e)
+		{
+			string wishlistEntry = textBox1.Text;
+
+			listBox2.Items.Add(wishlistEntry);
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
